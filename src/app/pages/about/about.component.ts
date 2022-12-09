af1a84cace035285e19beb40563088e3ffe44f2f
Toty-01 <caucheanthony@gmail.com>
2022-12-09 16:42:57 +0100

    continue

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone ,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  myObject = {
+    firstName: 'Nicolas',
+    name: 'Gulard',
+    adresse: '8 rue des hirondelles'
+  }
+  delivery = new Date()
+  name: string = "jean gulard";
+  intervalZone: any
+  counter = {
+    count: 0
+  }
+  increment() {
+    this.counter.count++
+  }
+  decrement() {
+    this.counter.count--
+  }
+  livraison = new Date(2020, 12, 25);
+  frenchFormat = 'dd MMMM yyyy';
+  usFormat = 'MMMM d, y';
 
-  ngOnInit(): void {
+  toggle = true;
+
+  get format()   {
+    return this.toggle ? this.frenchFormat : this.usFormat ;
   }
 
+  toggleFormat() { this.toggle = !this.toggle; }
+  genderMap: any = { male: "M.", female: "Mme." };
+
+  user = {
+    name: "Camille Delacour"
+  };
+
+  constructor(
+    private zone: NgZone
+  ) {}
+
+  ngOnInit() {
+    this.zone.runOutsideAngular(() => {
+        this.intervalZone = setInterval(() => {
+          this.ajouteZone()
+        }, 1000)
+    });
+  }
+  ajouteZone() {
+    this.counter.count++
+    console.log(this.counter.count)
+    if (this.counter.count >= 3) {
+      clearInterval(this.intervalZone)
+      this.zone.run(() => {})
+    }
+  }
 }
